fd4663212ab363c63e967dee1895bd00636cd370
William Santoso <williamsantoso93@yahoo.com>
2021-08-11 22:01:06 +0700

    Initial Commit

---
+++ Shared/The_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_GamesApp.swift
+//  Shared
+//
+//  Created by William Santoso on 11/08/21.
+//
+
+import SwiftUI
+
+@main
+struct The_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
